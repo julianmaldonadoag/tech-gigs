fe19298f1766dc17ccc0740ed913ecf4bc657224
Julian Maldonado <julianmaldonadoag@gmail.com>
2023-07-23 15:47:09 -0500

    Basic listings and linting views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// All listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'listings' => Listing::all()
+    ]);
+});
+
+// Single Listing
+Route::get('/listings/{id}', function($id) {
+    $listing = Listing::find($id);
+
+    if ($listing) {
+        return view('listing', [
+            'listing' => $listing
+        ]);
+    } else {
+        abort('404');
+    }
 });
