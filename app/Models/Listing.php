27ebe4de8566d7379af84c3e60b74aae60d3eccf
Julian Maldonado <julianmaldonadoag@gmail.com>
2023-07-25 18:03:36 -0500

    Listings belongs to user. Manage listings page

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -24,4 +24,9 @@ class Listing extends Model
                 ->orWhere('tags', 'like', $search);
         }
     }
+
+    // Relationship to User
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
