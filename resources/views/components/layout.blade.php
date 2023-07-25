1ff0fcdc47e6b58235881c6aa19d2ec4dc8d75e1
Julian Maldonado <julianmaldonadoag@gmail.com>
2023-07-25 11:33:12 -0500

    Register, login and auth validation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,16 +33,37 @@
             <img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" />
         </a>
         <ul class="flex space-x-6 mr-6 text-lg">
+            @auth
             <li>
-                <a href="register.html" class="hover:text-laravel">
+                <span class="font-bold uppercase">
+                    Welcome {{ auth()->user()->name }}
+                </span>
+            </li>
+            <li>
+                <a href="{{ url('/listings/manage') }}" class="hover:text-laravel">
+                    <i class="fa-solid fa-gear"></i> Manage Listings
+                </a>
+            </li>
+            <li>
+                <form class="inline" method="POST" action="{{ url('/logout') }}">
+                    @csrf
+                    <button type="submit">
+                        <i class="fa-solid fa-door-closed"></i> Logout
+                    </button>
+                </form>
+            </li>
+            @else
+            <li>
+                <a href="{{ url('/register') }}" class="hover:text-laravel">
                     <i class="fa-solid fa-user-plus"></i> Register
                 </a>
             </li>
             <li>
-                <a href="login.html" class="hover:text-laravel">
+                <a href="{{ url('/login') }}" class="hover:text-laravel">
                     <i class="fa-solid fa-arrow-right-to-bracket"></i> Login
                 </a>
             </li>
+            @endauth
         </ul>
     </nav>
 
